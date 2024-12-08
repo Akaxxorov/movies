692cafba7eed333123eb21cf5b83f5580142ac13
abduqahhorov Aziz <azizabduqahhorov07@gmail.com>
2024-12-08 11:02:26 +0000

    allowed_hosts

--- root/settings.py
+++ root/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-oi!oqscdbf&bpexankakx^_lsgo(q32*96$v!3$zrh5zjz-58j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['80.90.185.38' , 'http://80.90.185.38']
 
 
 # Application definition
