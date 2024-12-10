7109e86eae926a34490f392ba17337523e99dced
abduqahhorov Aziz <azizabduqahhorov07@gmail.com>
2024-12-10 18:04:38 +0000

    video fayllar

--- movies/management/commands/populate_movies.py
+++ movies/management/commands/populate_movies.py
@@ -54,25 +54,24 @@ class Command(BaseCommand):
 
     def create_movies(self):
         genres = list(Genre.objects.all())
-        for _ in range(25):  # Adjust the number of movies as needed
+        for i in range(15):  # Adjust the number of movies as needed
             movie = Movie.objects.create(
-                title=fake.sentence(nb_words=random.randrange(3, 7)),
+                title=Movie_title[i],
                 description=fake.paragraph(nb_sentences=random.randrange(5, 10)),
                 duration=random.randint(80, 180),
                 movie_type=fake.word(ext_word_list=["Action", "Romance", "Thriller", "Animation"]),
                 studios=fake.company(),
                 release_date=fake.date_between(start_date="-10y", end_date="today"),
-                rating=round(random.uniform(1, 10), 1),
-                views=random.randint(1000, 100000),
-                is_active=random.choice([True, False]),
+                rating=round(random.uniform(5, 10), 1),
+                views=random.randint(100, 10000),
                 quality=random.choice([choice[0] for choice in QualityChoices.choices]),
                 scores=round(random.uniform(0, 10), 1),
                 status=random.choice([choice[0] for choice in MovieStatusChoices.choices]),
                 type=random.choice([choice[0] for choice in MovieTypeChoices.choices]),
                 slug=None,  # Automatically generated in `save` method
-                image=random.choice(file_names),
-                video=random.choice(video_files),
-                thumb=fake.file_name(category="image"),
+                image=file_names[i],
+                video=video_files[i],
+                thumb=Movie_title[i],
             )
             movie.save()
 
